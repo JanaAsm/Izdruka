1bb0c3a60407991049019479f8fe81d084bfd1ef
JanaAsm <jana.asmenska@inbox.lv>
2021-01-07 10:36:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Var rakstīt komentārus
+#vairākās
+#rindās"""
+print("Sveika pasaule!")
+print(2*2)
+print(2*2, 2*3, 2*4,"Pitons")
+print(f"Ja saskaitīsim 5 at 7, iegūsim {5+7}.") #kombinēta izdruka - teksts un aprēķins
+print("Sveika, "+"pasaule!")
+print(5*"Jana ")
+print(365*60*60*24)
+print(0.1 + 0.2 - 0.3)
